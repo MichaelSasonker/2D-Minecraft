8833f312fb0751fbdd602c914b15fd3409f43318
Michael Sasonker <michaelsas1212@gmail.com>
2021-02-26 15:55:51 +0200

    game page #2

--- js/game.js
+++ js/game.js
@@ -5,6 +5,12 @@ const TILE_WIDTH = 40;
 let body = document.body;
 let gameCont = document.querySelector('.game-container');
 let tile = document.createElement('div');
+let pickaxe = document.querySelector('.pickaxe');
+let shovel = document.querySelector('.shovel');
+let axe = document.querySelector('.axe');
+let inventory = document.querySelector('.inventory');
+let tools = document.querySelectorAll('.tools-des');
+
 
 let gameHight = window.innerHeight;
 let gameWidth = gameCont.clientWidth;
@@ -48,7 +54,7 @@ function SetStone(matrix) {
     }
 
     for (let i = rows - 18; i < rows - 15; ++i) {
-        for (let j = 2; j < cols - 39; ++j) {
+        for (let j = 2; j < cols - 37; ++j) {
             matrix[i][j].classList.add('stone-tile');
         }
     }
@@ -67,7 +73,7 @@ function SetGrass(matrix) {
 function SetTreeWood(matrix) {
 
     for (let i = rows - 18; i < rows - 12; ++i) {
-        for (let j = 20; j < cols - 22; ++j) {
+        for (let j = 20; j < cols - 20; ++j) {
             matrix[i][j].classList.add('tree-wood-tile');
         }
     }
@@ -76,7 +82,7 @@ function SetTreeWood(matrix) {
 function SetTreeleaves(matrix) {
 
     for (let i = rows - 12; i < rows - 8; ++i) {
-        for (let j = 18; j < cols - 20; ++j) {
+        for (let j = 18; j < cols - 18; ++j) {
             matrix[i][j].classList.add('tree-leaves-tile');
         }
     }
@@ -85,29 +91,66 @@ function SetTreeleaves(matrix) {
 function SetCloud(matrix) {
 
     for (let i = rows - 4; i < rows - 2; ++i) {
-        for (let j = 26; j < cols - 14; ++j) {
+        for (let j = 26; j < cols - 11; ++j) {
             matrix[i][j].classList.add('cloud-tile');
         }
     }
 
     for (let i = rows - 2; i < rows - 1; ++i) {
-        for (let j = 27; j < cols - 15; ++j) {
+        for (let j = 27; j < cols - 12; ++j) {
             matrix[i][j].classList.add('cloud-tile');
         }
     }
 
     for (let i = rows - 4; i < rows - 3; ++i) {
-        for (let j = 30; j < cols - 13; ++j) {
+        for (let j = 30; j < cols - 10; ++j) {
             matrix[i][j].classList.add('cloud-tile');
         }
     }
 
     for (let i = rows - 4; i < rows - 3; ++i) {
-        for (let j = 25; j < cols - 17; ++j) {
+        for (let j = 25; j < cols - 10; ++j) {
             matrix[i][j].classList.add('cloud-tile');
         }
     }
 }
+
+// let toolsArr = [];
+// function AddToolToArr(arr) {
+//     for (let i = 0; i < tools.length - 2; ++i) {
+//         arr[i].push(tools[i]);
+//     }
+//     console.log('toolsArr is: ', arr);
+// }
+
+pickaxe.addEventListener('click', (e) => {
+    if (shovel.classList.contains('current-tool'))
+        shovel.classList.remove('current-tool');
+    else if (axe.classList.contains('current-tool')) 
+        axe.classList.remove('current-tool');
+
+    pickaxe.classList.add('current-tool');   
+});
+
+shovel.addEventListener('click', (e) => {
+    if (pickaxe.classList.contains('current-tool')) 
+        pickaxe.classList.remove('current-tool');
+    else if (axe.classList.contains('current-tool')) 
+        axe.classList.remove('current-tool');   
+
+    shovel.classList.add('current-tool'); 
+});
+
+axe.addEventListener('click', (e) => {
+    if (shovel.classList.contains('current-tool')) 
+        shovel.classList.remove('current-tool');
+    else if (pickaxe.classList.contains('current-tool')) 
+        pickaxe.classList.remove('current-tool');
+
+    axe.classList.add('current-tool');   
+});
+
+
 
 /*-------------------------------------------------------------------*/
 function MainFunction() {
@@ -121,5 +164,7 @@ function MainFunction() {
     SetTreeWood(gameArrRes);
     SetTreeleaves(gameArrRes);
     SetCloud(gameArrRes);
+
+    // AddToolToArr(toolsArr);
 
 }; MainFunction();
